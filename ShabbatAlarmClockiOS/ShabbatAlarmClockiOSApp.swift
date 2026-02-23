2591a5e1bc06aa1eba453ba467fc7ee6f63b3f43
Jeremy Jacob <jeremyjacob101@gmail.com>
2026-02-22 19:36:49 -0500

    Initial Commit

---
+++ ShabbatAlarmClockiOS/ShabbatAlarmClockiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShabbatAlarmClockiOSApp.swift
+//  ShabbatAlarmClockiOS
+//
+//  Created by Jeremy Jacob on 22/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct ShabbatAlarmClockiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
